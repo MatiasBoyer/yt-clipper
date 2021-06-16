db4928064269219b2af69d9fadf209b0138ded10
Matías <matiasboyer7@hotmail.com>
2021-06-16 17:07:49 -0300

    better loop

--- runserver.py
+++ runserver.py
@@ -20,37 +20,33 @@ def checkUpdates():
     return False
 
 server = None
-def openServer():
+async def openServer():
     server = Popen(["npm", "run", "dev"], shell=True)
-def closeServer():
+async def closeServer():
     global server
     if server != None:
         server.kill()
         server.terminate()
         server = None
+        await asyncio.sleep(5)
 
 async def check_for_updates():
     await asyncio.sleep(10)
-    needToUpdate = checkUpdates()
-    if needToUpdate == True:
-        closeServer()
-        while server != None:
-            await asyncio.sleep(1)
+    return checkUpdates()    
 
-        exec_cmd("git reset --hard origin/prod")
-
-        await asyncio.sleep(15)
-        openServer()
-
-async def forever_update():
+async def main():
+    await openServer()
     while True:
-        await check_for_updates()
+        needToUpdate = await check_for_updates()
+        if needToUpdate:
+            await closeServer()
+            exec_cmd("git reset --hard origin/prod")
+            await openServer()
 
 loop = asyncio.get_event_loop()
-task = loop.create_task(forever_update())
+task = loop.create_task(main())
 
 try:
-    openServer()
     loop.run_until_complete(task)
     loop.run_forever()
 except asyncio.CancelledError:
