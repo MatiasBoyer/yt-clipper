d797c8766be8e581c646d39f9f247b3b45a79ad8
Matías <matiasboyer7@hotmail.com>
2021-06-16 23:51:03 -0300

    movida la config y errores a archivos separados

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = 
+{
+    PORT:                   80,
+    MAX_VIDEO_DURATION:     300,
+
+    LIMIT_TIME:             15,
+    LIMIT_CALLS:            100,
+    
+    ADEL_OLDER_THAN:        15,
+    ADEL_EVERY_MINS:        20
+}
